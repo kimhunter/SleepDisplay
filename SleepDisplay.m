cd5c64a8a755fdce7a7eb23cc958a0507027b715
Kim Hunter <bigkm1@gmail.com>
2011-09-27 09:23:10 +1000

    Added wake functionality

--- SleepDisplay.m
+++ SleepDisplay.m
@@ -1,17 +1,24 @@
 #import <Foundation/Foundation.h>
 #import <IOKit/IOKitLib.h>
-
+#import <string.h>
 
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    BOOL shouldWake = NO;
+
+    if (argc > 1)
+    {
+        NSString *flag = [NSString stringWithUTF8String:argv[1]];
+        shouldWake = [flag isEqualToString:@"-wake"];
+    }
+    
 	io_registry_entry_t entry = IORegistryEntryFromPath(kIOMasterPortDefault, "IOService:/IOResources/IODisplayWrangler");
 	if(entry)
 	{
-		IORegistryEntrySetCFProperty(entry, CFSTR("IORequestIdle"), kCFBooleanTrue);
+		IORegistryEntrySetCFProperty(entry, CFSTR("IORequestIdle"), shouldWake ? kCFBooleanFalse : kCFBooleanTrue);
 		IOObjectRelease(entry);
 	}
-	
-	
 	
     [pool drain];
     return 0;
